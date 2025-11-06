d2a779e9c483c25dcda3e0104563b9d7fab5aece
Ezra Abrari <ezraabrari@gmail.com>
2025-11-06 13:09:39 +0700

    Membuat fungsi form isian

--- app/src/main/java/com/shophub/questnavigasiui_059/view/Formulir.kt
+++ app/src/main/java/com/shophub/questnavigasiui_059/view/Formulir.kt
@@ -1,2 +1,11 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.shophub.questnavigasiui_059.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormIsian(
+
+)
