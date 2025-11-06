41abc1665f1bc253c59ba686854f32bf9a7ff09f
Ezra Abrari <ezraabrari@gmail.com>
2025-11-06 13:54:01 +0700

    Membuat scaffold

--- app/src/main/java/com/shophub/questnavigasiui_059/view/TampilData.kt
+++ app/src/main/java/com/shophub/questnavigasiui_059/view/TampilData.kt
@@ -1,7 +1,11 @@
 package com.shophub.questnavigasiui_059.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import com.shophub.questnavigasiui_059.R
 
@@ -15,4 +19,17 @@ fun TampilData(
         Pair(stringResource(R.string.jenis_kelamin), "Lainnya"),
         Pair(stringResource(R.string.alamat), "Yogyakarta")
     )
+    Scaffold(modifier = Modifier,
+        {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(R.string.tampil),
+                        color = Color.White
+                    )
+                },
+            )
+        }){ isiRuang->
+
+    }
 }
