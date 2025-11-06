d1911829c1449be0b4f5b5d21902263a7a2dae8d
Ezra Abrari <ezraabrari@gmail.com>
2025-11-06 13:13:50 +0700

    Membuat jenis kelamin untuk laki laki dan perempuan dan onsubmit dll untuk ke halaman selanjutnya dan membuat scaffold untuk halaman sebesar satu layar handphone yang dimulai dari top bar

--- app/src/main/java/com/shophub/questnavigasiui_059/view/Formulir.kt
+++ app/src/main/java/com/shophub/questnavigasiui_059/view/Formulir.kt
@@ -4,8 +4,13 @@ package com.shophub.questnavigasiui_059.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun FormIsian(
-
-)
+    jenisK: List<String> = listOf("Laki-laki", "Peremuan"),
+    OnSubmitBtnClick : () -> Unit
+){
+    Scaffold (modifier= Modifier,
+        )
+}
