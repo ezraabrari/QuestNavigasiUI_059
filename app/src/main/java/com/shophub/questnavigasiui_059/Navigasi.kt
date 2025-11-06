a8bc5acf31625691a6a74a7c4c7b9338f9ca9cc8
Ezra Abrari <ezraabrari@gmail.com>
2025-11-06 12:10:14 +0700

    Membuat halaman formulir di awal saat memulai app

--- app/src/main/java/com/shophub/questnavigasiui_059/Navigasi.kt
+++ app/src/main/java/com/shophub/questnavigasiui_059/Navigasi.kt
@@ -1,6 +1,8 @@
 package com.shophub.questnavigasiui_059
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import java.lang.reflect.Modifier
@@ -15,5 +17,13 @@ fun DataApp(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier
 ){
+    Scaffold { isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+
+
+        )
+    }
 
 }
